f37382c99ac45682cbf2deb85163cf69e2f48423
daryltchuitio <daryltchuitio@gmail.com>
2026-02-14 18:41:20 +0100

    Petite modifs dans connexion.html et server.js pour que les dashboards actuels (qui utilisent des rôles en français) soient compatibles avec la nouvelle gestion des rôles côté backend (en anglais). Pour que les dashboards ne force plus le renvoie vers connexion.html en cas de rôle non reconnu, j’ai donc ajouté une redirection basée sur le rôle FR (compatible avec les rôles actuels des dashboards) dans connexion.html.

--- backend/server.js
+++ backend/server.js
@@ -103,7 +103,8 @@ app.post("/api/login", async (req, res) => {
       user: {
         id: user._id,
         name: user.name,
-        email: user.email
+        email: user.email,
+        role: user.role
       }
     });
 
